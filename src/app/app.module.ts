51eb6c419d144bf03efae8d274bb168e46ce81da
Chema Torres <chemaaa94@gmail.com>
2022-12-07 10:11:20 -0600

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeScreenComponent } from './components/home-screen/home-screen.component';
+import { RequestFormComponent } from './components/request-form/request-form.component';
+import { RequestViewComponent } from './components/request-view/request-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeScreenComponent,
+    RequestFormComponent,
+    RequestViewComponent
   ],
   imports: [
     BrowserModule,
