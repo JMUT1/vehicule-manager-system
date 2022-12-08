ff739ae963adfb15b2ec62f94092ddc19bf1d3f1
Chema Torres <chemaaa94@gmail.com>
2022-12-08 08:55:10 -0600

    config routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeScreenComponent } from './components/home-screen/home-screen.component';
+import { RequestFormComponent } from './components/request-form/request-form.component';
+import { RequestViewComponent } from './components/request-view/request-view.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+  {path: 'home-screen', component:HomeScreenComponent },
+  {path: 'client-form', component:RequestFormComponent },
+  {path: 'view-client-info', component:RequestViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
