3ee142604885b5ec91ef69be0128f0e6a856497f
Chema Torres <chemaaa94@gmail.com>
2022-12-08 09:39:44 -0600

    localStorage working

--- src/app/components/request-form/request-form.component.ts
+++ src/app/components/request-form/request-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import{User} from "../../shared/info-client-model"
+import {ServiceVehiculeDataService} from '../../service-vehicule-data.service'
 
 @Component({
   selector: 'app-request-form',
@@ -11,7 +12,7 @@ export class RequestFormComponent implements OnInit {
   form: FormGroup
   userInfo: User
 
-  constructor(private formBuiler: FormBuilder) {
+  constructor(private formBuiler: FormBuilder, private vehiculeInfoService: ServiceVehiculeDataService ) {
     this.buildForm()
    }
 
@@ -32,7 +33,11 @@ export class RequestFormComponent implements OnInit {
 
 SaveSubmit(event: Event){
   event.preventDefault()
-  console.log(this.form.value);
+  if(this.form.valid){
+    console.log(this.form.value);
+    this.vehiculeInfoService.addUser(this.form.value)
+
+  }
 }
 
 
--- src/app/service-vehicule-data.service.ts
+++ src/app/service-vehicule-data.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {User} from './shared/info-client-model'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceVehiculeDataService {
 
-  constructor() { }
+  userInfo: User[]
+
+  constructor() {
+    this.userInfo = []
+   }
+
+   addUser(info: User){
+    this.userInfo = [];
+    if(localStorage.getItem('Users') === null){
+      this.userInfo.push(info);
+      localStorage.setItem('Users', JSON.stringify(this.userInfo))
+    }else{
+      this.userInfo.push(info)
+      this.userInfo = JSON.parse(localStorage.getItem('Users')!)
+      this.userInfo.push(info)
+      localStorage.setItem('Users', JSON.stringify(this.userInfo))
+    }
+  }
 }
+
